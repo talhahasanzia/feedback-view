1f9e0e8b62b5e3ec5295fd65114b48826c6931f0
Talha <talha.zia>
2019-11-26 02:33:08 +0500

    added library from jitpack

--- app/src/main/java/com/singledraft/radiogroupdemo/MainActivity.java
+++ app/src/main/java/com/singledraft/radiogroupdemo/MainActivity.java
@@ -14,9 +14,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FeedbackView feedbackView= findViewById(R.id.feedback_view);
+        // set feedback question
         feedbackView.setQuery("How do you do? ");
+
+        // set options gap (Optional)
         feedbackView.setOptionsGap(R.dimen.my_app_options_gap);
 
+        // 25 dummy options as number
         for(int i=0; i<25; i++){
             feedbackView.addOption(String.valueOf(i));
         }
